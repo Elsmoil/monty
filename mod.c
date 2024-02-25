8b50147559cd73f556c5b59d39aa305dfb850c25
Elsmoil <alsmoilalrasta999@gmail.com>
2024-02-25 07:43:11 -0800

    more

--- mod.c
+++ mod.c
@@ -1,11 +1,12 @@
 #include "monty.h"
 /**
- * f_dive - divides the top two elements of the stack.
+ * f_mod - computes the rest of the division of the second
+ * top element of the stack by the top element of the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_dive(stack_t **head, unsigned int counter)
+void f_mod(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
@@ -18,8 +19,8 @@ void f_dive(stack_t **head, unsigned int counter)
 	}
 	if (len < 2)
 	{
-		f_printf(stderr, "L%d: can't div, stack too short\n", counter);
-		f_close(bus.file);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
@@ -27,13 +28,13 @@ void f_dive(stack_t **head, unsigned int counter)
 	h = *head;
 	if (h->n == 0)
 	{
-		f_printf(stderr, "L%d: division by zero\n", counter);
-		f_close(bus.file);
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = h->next->n / h->n;
+	aux = h->next->n % h->n;
 	h->next->n = aux;
 	*head = h->next;
 	free(h);
--- pop.c
+++ pop.c
@@ -1,17 +1,17 @@
 #include "monty.h"
 /**
- * f_ptop - prints the top
+ * f_pop - prints the top
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_ptop(stack_t **head, unsigned int counter)
+void f_pop(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d: can't ptop an empty stack\n", counter);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
